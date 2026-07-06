a2f51d29aec59a8ec391059e4e7f3741cf2e9b09
Pedro Henrique Freire de Oliveira <pedrobolina123@gmail.com>
2026-07-06 06:40:25 -0300

    Initial Commit

---
+++ Chef Delivery 2/Chef_Delivery_2App.swift
@@ -0,0 +1,17 @@
+//
+//  Chef_Delivery_2App.swift
+//  Chef Delivery 2
+//
+//  Created by Pedro Henrique Freire de Oliveira on 06/07/26.
+//
+
+import SwiftUI
+
+@main
+struct Chef_Delivery_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
